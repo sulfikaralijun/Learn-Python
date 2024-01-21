96968d170ed24903a515821c0277eb90bc2168fa
Sulfikar Alijun <sulfikaralijun@gmail.com>
2024-01-21 21:33:31 +0800

    update kalkulator.py

--- src/problem/kalkulator/kalkulator.py
+++ src/problem/kalkulator/kalkulator.py
@@ -2,3 +2,10 @@ print("======== Selamat datang di kalkulator Sulfikar Alijun ========")
 
 angka_pertama = input("Masukkan angka pertama: ")
 angka_kedua = input("Masukkan angka kedua: ")
+jenis_operasi = input("""Pilih operasi yang anda inginkan:
+1. Penjumlah
+2. Pengurangan
+3. Perkalian
+4. Pembagian
+
+Masukkan nomor operasi yang anda inginkan (1/2/3/4): """)
