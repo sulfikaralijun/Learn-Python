842b982e67688a52f61f202aa4cab0840a4515be
Sulfikar Alijun <116356678+sulfikaralijun@users.noreply.github.com>
2024-01-22 11:57:12 +0800

    update src/problem/kalkulator/kalkulator.py

--- src/problem/kalkulator/kalkulator.py
+++ src/problem/kalkulator/kalkulator.py
@@ -2,13 +2,13 @@ print("======== Selamat datang di kalkulator Sulfikar Alijun ========")
 
 angka_pertama = input("Masukkan angka pertama: ")
 angka_kedua = input("Masukkan angka kedua: ")
-jenis_operasi = input("""Pilih operasi yang anda inginkan:
+jenis_operasi = int(input("""Pilih operasi yang anda inginkan:
 1. Penjumlah
 2. Pengurangan
 3. Perkalian
 4. Pembagian
 
-Masukkan nomor operasi yang anda inginkan (1/2/3/4): """)
+Masukkan nomor operasi yang anda inginkan (1/2/3/4): """))
 if jenis_operasi == 1 :
   hasil = angka_pertama + angka_kedua
 elif jenis_operasi == 2 :
