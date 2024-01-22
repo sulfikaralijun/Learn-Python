6bf83eddb108400945c7e25f7546636476ae8db1
Sulfikar Alijun <116356678+sulfikaralijun@users.noreply.github.com>
2024-01-22 11:33:59 +0800

    update src/problem/kalkulator/kalkulator.py

--- src/problem/kalkulator/kalkulator.py
+++ src/problem/kalkulator/kalkulator.py
@@ -11,3 +11,11 @@ jenis_operasi = input("""Pilih operasi yang anda inginkan:
 Masukkan nomor operasi yang anda inginkan (1/2/3/4): """)
 if jenis_operasi == 1 :
   hasil = angka_pertama + angka_kedua
+elif jenis_operasi == 2 :
+  hasil = angka_pertama - angka_kedua
+elif jenis_operasi == 3 :
+  hasil = angka_pertama * angka_kedua
+else :
+  hasil = angka_pertama / angka_kedua
+
+print("Hasil: " + hasil)
