dd206a07ce262ce2de44e19085c1b7e44863ab0e
Sulfikar Alijun <sulfikaralijun@gmail.com>
2024-01-22 19:03:17 +0800

    update kalkulator.py

--- src/problem/kalkulator/kalkulator.py
+++ src/problem/kalkulator/kalkulator.py
@@ -1,10 +1,10 @@
 print("======= KALKULATOR ========")
 
 # mengambil input angka pertama
-angka_pertama = int(input("Masukkan angka pertama: "))
+# angka_pertama = int(input("Masukkan angka pertama: "))
 
-# mengambil input angka kedua
-angka_kedua = int(input("Masukkan angka kedua: "))
+# # mengambil input angka kedua
+# angka_kedua = int(input("Masukkan angka kedua: "))
 
 # mengambil input jenis operasi
 def input_jenis_operasi():
@@ -17,6 +17,7 @@ def input_jenis_operasi():
   Masukkan nomor operasi yang anda inginkan (1/2/3/4): """))
 
 input_jenis_operasi()
+print(input_jenis_operasi())
 
 if jenis_operasi == 1 :
   hasil = angka_pertama + angka_kedua
