f0deabd5861e679fbffaffbb5c0674a45971852a
Sulfikar Alijun <sulfikaralijun@gmail.com>
2024-01-22 21:33:07 +0800

    update main.py

--- src/main.py
+++ src/main.py
@@ -3,3 +3,10 @@ print(name) # output: sulfikar
 
 name = "sulfikar" # menggunakan double quote
 print(name) # output: sulfikar
+
+alamat = """
+Jl. Pahlawan No. 1
+Jakarta
+Indonesia
+""" # menggunakan multiline stirng
+print(alamat)
