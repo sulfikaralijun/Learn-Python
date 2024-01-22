f33d7e7e52046b3ae0f2c5f68cf2e2d0093342d0
Sulfikar Alijun <sulfikaralijun@gmail.com>
2024-01-22 19:01:23 +0800

    update kalkulator.py

--- src/problem/kalkulator/kalkulator.py
+++ src/problem/kalkulator/kalkulator.py
@@ -7,14 +7,17 @@ angka_pertama = int(input("Masukkan angka pertama: "))
 angka_kedua = int(input("Masukkan angka kedua: "))
 
 # mengambil input jenis operasi
-jenis_operasi = int(input("""Pilih operasi yang anda inginkan:
-1. Penjumlah
-2. Pengurangan
-3. Perkalian
-4. Pembagian
+def input_jenis_operasi():
+  jenis_operasi = int(input("""Pilih operasi yang anda inginkan:
+  1. Penjumlah
+  2. Pengurangan
+  3. Perkalian
+  4. Pembagian
 
-Masukkan nomor operasi yang anda inginkan (1/2/3/4): """))
-  
+  Masukkan nomor operasi yang anda inginkan (1/2/3/4): """))
+
+
+
 if jenis_operasi == 1 :
   hasil = angka_pertama + angka_kedua
   operasi = 'Penjumlahan'
