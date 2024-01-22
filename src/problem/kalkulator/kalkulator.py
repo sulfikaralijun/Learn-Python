5f8e0721e22905c74070cda7cd6c252af281a7b9
Sulfikar Alijun <sulfikaralijun@gmail.com>
2024-01-22 18:55:16 +0800

    update kalkulator.py

--- src/problem/kalkulator/kalkulator.py
+++ src/problem/kalkulator/kalkulator.py
@@ -1,5 +1,6 @@
-print("======== Selamat datang di kalkulator Sulfikar Alijun ========")
+print("======= KALKULATOR ========")
 
+# mengambil
 angka_pertama = int(input("Masukkan angka pertama: "))
 angka_kedua = int(input("Masukkan angka kedua: "))
 jenis_operasi = int(input("""Pilih operasi yang anda inginkan:
@@ -9,6 +10,7 @@ jenis_operasi = int(input("""Pilih operasi yang anda inginkan:
 4. Pembagian
 
 Masukkan nomor operasi yang anda inginkan (1/2/3/4): """))
+  
 if jenis_operasi == 1 :
   hasil = angka_pertama + angka_kedua
 elif jenis_operasi == 2 :
