4c58d2b6fdcc294aa7a89a54d850c77f7e192f4f
Sulfikar Alijun <sulfikaralijun@gmail.com>
2024-01-23 21:05:12 +0800

    update syntax.py

--- src/1 - Python Basic/Fundamental/Syntax/syntax.py
+++ src/1 - Python Basic/Fundamental/Syntax/syntax.py
@@ -3,3 +3,6 @@
 if True == True:
   print("Hello World!") # jika ingin menambahkan kode yang bersarang di code lain, harus menggunakan indentasi
 
+if a == True and b == False and \
+    c == True: # jika ingin menulis statement lebih dari satu baris, bisa menambahkan back slash '\'
+  print("Continuation of statements")
