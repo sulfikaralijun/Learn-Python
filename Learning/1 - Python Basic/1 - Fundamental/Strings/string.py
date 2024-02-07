03a974bbacc7b80c9e547a28e454df10952a1ae2
Sulfikar Alijun <sulfikaralijun@gmail.com>
2024-02-07 22:11:25 +0800

    update string.py

--- Learning/1 - Python Basic/1 - Fundamental/Strings/string.py
+++ Learning/1 - Python Basic/1 - Fundamental/Strings/string.py
@@ -1,2 +1,6 @@
+# String ditandai dengan tanda kutip, baik itu tanda kutip tunggal
+# atau tanda kutip ganda.
 message = 'Ini adalah string'
 message = "Ini juga adalah sebuah string"
+
+print(message) # Ini juga adalah sebuah string
