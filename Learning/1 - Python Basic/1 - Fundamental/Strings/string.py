584508b798144defbabcf95d39ff2078eb9f487c
Sulfikar Alijun <sulfikaralijun@gmail.com>
2024-02-07 22:14:22 +0800

    update string.py

--- Learning/1 - Python Basic/1 - Fundamental/Strings/string.py
+++ Learning/1 - Python Basic/1 - Fundamental/Strings/string.py
@@ -1,6 +1,10 @@
-# String ditandai dengan tanda kutip, baik itu tanda kutip tunggal
-# atau tanda kutip ganda.
+# String ditandai dengan tanda kutip, baik itu
+# tanda kutip tunggal atau tanda kutip ganda.
 message = 'Ini adalah string'
 message = "Ini juga adalah sebuah string"
 
 print(message) # Ini juga adalah sebuah string
+
+# Jika string berisi tanda kutip tunggal, maka gunakan 
+# tanda kutip ganda untuk membungkus string, begitupun sebaliknya
+message = ""
